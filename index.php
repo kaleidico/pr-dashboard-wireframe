7c8d4fa358743b8669e2b1db22eec66b21a330b1
Charles Pobee-Mensah <charles.pobeemensah@kaleidico.com>
2016-05-05 17:09:24 -0400

    First set of styling and html

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-hello world
+<head>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		header
+		<i class="fa fa-user" aria-hidden="true"></i>
+	</header>
+		<div class="container">
+			<div class="col-md-9">
+				<div class="overview shadow">
+					<img src="http://placehold.it/400x327">
+					<div class="overview-details">
+						<h2>$920,000</h2>
+						<span>4121 Ledgestone Drive</span><br>
+						<span>Troy, MI 48098</span><br>
+						<hr>
+						<ul>
+							<li><b>Bedrooms:</b> 4</li>
+							<li><b>Bathrooms:</b> 4.5</li>
+							<li><b>Type Single:</b> Family Home</li>
+							<li><b>Square Feet:</b> 4,6000</li>
+						</ul>
+					</div><!-- end: overview-details -->
+				</div><!-- end: overview -->
+				<div class="shadow">
+				 test
+				</div>
+			</div><!-- end: col-md-9 -->
+			<div class="col-md-3">
+				<div class="sidebar">
+					<div class="status">status: draft</div>
+					<a href="#" class="btn btn-default">Publish</a>
+				</div>
+			</div><!-- end: col-md-3 -->
+		</div><!-- end: container -->
+<!-- Bootstrap Javascript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
